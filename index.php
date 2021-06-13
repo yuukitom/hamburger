0e688355f650dec3117a96a779141d43c0befa9f
yuki <65431748+yuukitom@users.noreply.github.com>
2021-06-13 17:36:00 +0900

    first-commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="description" content="ハンバーガーサイト">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hamburger</title>
+    <link rel="stylesheet" href="css/ress.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://kit.fontawesome.com/ded60250de.js" crossorigin="anonymous"></script>
+  </head>
+  <body>
+      <header class="l-header p-header">
+        <h1 class="p-header__title u-p-header__title--pcm0 u--mt20"><a href="index.html"><img src="img/logo.svg" alt="logo"></a></h1>
+        <nav class="p-header__nav">
+        </nav>
+      </header>
+
+      <div class="p-mainvisual u--mb80">
+        <picture>
+          <source media="(min-width: 600px)" srcset="img/mainvisual-pc.jpg">
+          <img src="img/mainvisual-sp.jpg" alt="mainvisual">
+        </picture>
+      </div>
+      
+      <main class="l-main p-main">
+        <article>
+          <section class="p-article__wrapper">
+          </section>
+
+          
+
+        </article>
+      </main>
+      
+
+      <footer class="l-footer p-footer"></footer>
+  </body>
+</html>
