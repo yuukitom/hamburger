8e897ab7ba95d4f11a33a2b022096f5112f53a7c
yuki <65431748+yuukitom@users.noreply.github.com>
2021-07-08 23:15:15 +0900

    20210708_アーカイブ_アーカイブと検索アーカイブ完成

--- js/script.js
+++ js/script.js
@@ -3,4 +3,12 @@ $(document).ready(function(){
     $(".p-container__right").toggleClass("is-open");
     $("body").toggleClass("is-open");
   });
+
+  $(".p-pagination__num").on("click", function(){
+    $(".current").removeClass("current");
+    $(this).addClass("current");
+
+    return false;
+  });
+
 });
